23827c4bfbb9d35c8e8353d45b09bc80404d7a58
german <germanwibaux@gmail.com>
2018-06-01 21:00:22 -0300

    Funcionamiento completo

--- src/Ejercicio_7.java
+++ src/Ejercicio_7.java
@@ -1,20 +1,15 @@
 
 public class Ejercicio_7 {
 	
-	public static boolean esHoja(int[][] hoja) {
-		int i=0;
-		int j=0;
+	public static boolean esHoja(int[][] hoja) {	
 		boolean control = true;
-		while ((control) && (i < hoja.length)) {
-			while ((control) && (j < hoja.length)) {
-				if ( hoja[i][j] == -1 ) {
+		for(int i=0; i<hoja.length; i++) {
+			for(int j=0;j<hoja.length; j++) {
+				if (hoja[i][j] == -1) {
 					control = false;
 				}
-				j++;
-			}
-			i++;
+			} 
 		}
-		//imprimirTablero(hoja);
 		return control;
 	}
 	
@@ -22,10 +17,9 @@ public class Ejercicio_7 {
 		int parcial = 0;
 		for(int i=0; i<tablero.length; i++) {
 			for(int j=0;j<tablero.length; j++) {
-				parcial += tablero[i][j];
-			}
-		}
-		System.out.print(parcial + " " + total + "\n");
+				parcial = parcial + tablero[i][j]; 
+			} 
+		}	
 		return parcial == total;
 	}
 	
@@ -34,7 +28,7 @@ public class Ejercicio_7 {
 			for(int j=0;j<tablero[i].length; j++) {
 				System.out.print("|" + tablero[i][j] + "|");			
 			}		
-			System.out.println();
+			System.out.println(); 
 		}
 		
 	}
@@ -43,28 +37,29 @@ public class Ejercicio_7 {
 		return usados[i -1] == true;
 	}
 	
-	public static void backTablero(int[][] tablero, boolean[] usados, int total, int k, int fila, int columna ) {		
-		if(esHoja(tablero)) {
+	public static void backTablero(int[][] tablero, boolean[] usados, int total, int k, int fila, int columna , boolean control ) {		
+		if( esHoja(tablero) && (!control)) {
 			if (esSolucion(tablero, total)) {
 				imprimirTablero(tablero);
+				control = true;
 			}
-		} else {
+		} else if (!control) {
 			for(int i=1;i<=k;i++) {
 				if(estaDisponible(i, usados))  {
-					tablero[fila][columna] = i;
-					usados[i - 1] = false;
-					if(columna < tablero.length-1 ) {
-						int proximo = columna + 1;
-						backTablero(tablero, usados, total, k, fila, proximo);
-					}					
-					if (fila < tablero.length-1 ) {
-						int proximo = fila + 1;
-						backTablero(tablero, usados, total, k, proximo, 0);						
-					}
-					tablero[fila][columna] = -1;
+					if (fila < tablero.length && columna < tablero.length) {
+						tablero[fila][columna] = i;
+						usados[i - 1] = false;
+					}			
+					if(columna < tablero.length ) {
+						backTablero(tablero, usados, total, k, fila, columna + 1, control);
+					} else if (fila < tablero.length ) {			
+						backTablero(tablero, usados, total, k, fila + 1, 0, control);					
+					}									
+				}	
+				if (fila < tablero.length && columna < tablero.length) {
+					tablero[fila][columna] = -1; 
 					usados[i - 1] = true;
 				}				
-				
 			}
 		}
 		
@@ -72,10 +67,10 @@ public class Ejercicio_7 {
 
 	public static void main(String[] args) {
 		//Inicializamos las variables necesarias
-		int k = 15;
+		int k = 10;
 		int n = 3;
 		int[][] tableroMagico = new int[n][n];
-		boolean[] valoresAUtilizar = new boolean[k];
+		boolean[] valoresAUtilizar = new boolean[k]; 
 		
 		for(int i=0; i<tableroMagico.length; i++) {
 			for(int j=0; j<tableroMagico.length; j++) {
@@ -85,11 +80,10 @@ public class Ejercicio_7 {
 		
 		for(int i=0; i<valoresAUtilizar.length; i++) {
 			valoresAUtilizar[i] = true;
-		}	
+		}			
 		
-		
-		backTablero(tableroMagico, valoresAUtilizar, 25, k, 0, 0);
-		//imprimirTablero(tableroMagico);
+		backTablero(tableroMagico, valoresAUtilizar, 60, k, 0, 0, false);
+		imprimirTablero(tableroMagico);
 		
 	}
 
